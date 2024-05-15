3bc2c05d5731d841a36580b5306af4546ea06d31
Kamran <kamran.abish@testzap.in>
2024-05-15 19:54:48 +0530

    chainlit working

--- chinook/main.py
+++ chinook/main.py
@@ -128,15 +128,29 @@ tools = [
 ]
 
 
-messages = []
-messages.append({"role": "system", "content": "Answer user questions by generating SQL queries against the Chinook Music Database."})
-messages.append({"role": "user", "content": "Hi, who are the top 5 artists by number of tracks?"})
-chat_response = chat_completion_request(messages, tools)
-assistant_message = chat_response.choices[0].message
-assistant_message.content = str(assistant_message.tool_calls[0].function)
-messages.append({"role": assistant_message.role, "content": assistant_message.content})
-if assistant_message.tool_calls:
-    results = execute_function_call(assistant_message)
-    messages.append({"role": "function", "tool_call_id": assistant_message.tool_calls[0].id, "name": assistant_message.tool_calls[0].function.name, "content": results})
-pretty_print_conversation(messages)
 
+
+import chainlit as cl
+
+@cl.on_chat_start
+async def on_chat_start():
+    pass
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    messages = []
+    messages.append({"role": "system", "content": "Answer user questions by generating SQL queries against the Chinook Music Database."})
+    messages.append({"role": "user", "content": message.content})
+    chat_response = chat_completion_request(messages, tools)
+    assistant_message = chat_response.choices[0].message
+    assistant_message.content = str(assistant_message.tool_calls[0].function)
+    messages.append({"role": assistant_message.role, "content": assistant_message.content})
+    if assistant_message.tool_calls:
+        results = execute_function_call(assistant_message)
+        messages.append({"role": "function", "tool_call_id": assistant_message.tool_calls[0].id, "name": assistant_message.tool_calls[0].function.name, "content": results})
+    pretty_print_conversation(messages)
+
+    msg = cl.Message(content=results)
+
+    await msg.send()
